0c641a4358eeddc18527b5c616eba87e39702dec
Erik Nilsen <enilsen16@live.com>
2016-07-16 21:40:04 -0700

    remove un-needed test

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ApiAi.Mixfile do
 
   def project do
     [app: :ex_api_ai,
-     version: "0.1.0-beta.0",
+     version: "0.1.0-beta.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
--- test/api_ai_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ApiAiTest do
-  use ExUnit.Case
-  doctest ApiAi
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
