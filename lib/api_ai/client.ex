d785b6a3349fba0777bc28c7f91b5b0cf6e728b1
Micah Wedemeyer <me@micahwedemeyer.com>
2017-06-27 15:09:39 -0400

    Move HTTP work to Client

--- lib/api_ai/client.ex
+++ lib/api_ai/client.ex
@@ -2,7 +2,7 @@ defmodule ApiAi.Client do
   defstruct [:client_access_token, :version, :base_url]
   @enforce_keys [:client_access_token, :version, :base_url]
 
-  @base_url "https://api.api.ai/v1/"
+  @base_url "https://api.api.ai/v1"
   @version "20160707"
 
   def new(%{} = params \\ %{}) do
@@ -15,7 +15,21 @@ defmodule ApiAi.Client do
     } |> Map.merge(params)
   end
 
-  def perform(method, url, body \\ "", headers \\ [], options \\ []) do
-    HTTPoison.request(method, url, body, headers, options)
+  def perform(%__MODULE__{} = client, method, path, body \\ "", headers \\ [], options \\ []) do
+    query_string = %{
+      v: client.version
+    }
+
+    uri = URI.parse(client.base_url)
+    |> Map.update!(:path, &(&1 <> path))
+    |> Map.put(:query, URI.encode_query(query_string))
+    |> URI.to_string
+
+    headers = [
+      {"Authorization", "Bearer #{client.client_access_token}"},
+      {"Content-Type", "application/json; charset=utf-8"}
+    ] ++ headers
+
+    HTTPoison.request(method, uri, body, headers, options)
   end
 end
--- lib/api_ai/text_request.ex
+++ lib/api_ai/text_request.ex
@@ -4,15 +4,12 @@ defmodule ApiAi.TextRequest do
   def text_request(text, options \\ %{}) when is_binary(text), do: Client.new() |> text_request(text, options)
   def text_request(%Client{} = client, text) when is_binary(text), do: text_request(client, text, %{})
   def text_request(%Client{} = client, text, %{} = options) do
-    url = client.base_url <> "query?v=" <> client.version
     body =
       %{"query" => text, "lang" => "en"}
       |> merge_with_body(options)
       |> Poison.encode!
 
-    headers = [{"Authorization", "Bearer #{client.client_access_token}"}, {"Content-Type", "application/json; charset=utf-8"}]
-
-    case Client.perform(:post, url, body, headers) do
+    case Client.perform(client, :post, "/query", body, []) do
       {:ok, %HTTPoison.Response{} = response} ->
         response.body |> Poison.Parser.parse!
       {:error, response} ->
